5867bfcd5ab8c3c37826ce5884a58f39331aa3b9
Jimin Kim <0305jimin@gmail.com>
2023-11-20 03:00:26 +0900

    Feat: FileFindVisitor using Visitor pattern

--- DP3/src/Visitor/VisitorProblem/FileFindVisitor.java
+++ DP3/src/Visitor/VisitorProblem/FileFindVisitor.java
@@ -1,10 +1,31 @@
 package Visitor.VisitorProblem;
 
+import java.util.Iterator;
+
 public class FileFindVisitor extends Visitor {
-    public void visit(File file) {
+    private String fileExtension;
+    private String currentdir = "";
 
+    public FileFindVisitor (String fileExtension) {
+        this.fileExtension = fileExtension;
     }
-    public void visit(Directory directory) {
 
+    @Override
+    public void visit(File file) {
+        String fileName = file.getName();
+        if (fileName.endsWith(fileExtension))
+            System.out.println(currentdir + "/" + file);
+    }
+
+    @Override
+    public void visit(Directory directory) {
+        String savedir = currentdir;
+        currentdir = currentdir + "/" + directory.getName();
+        Iterator it = directory.iterator();
+        while (it.hasNext()) {
+            Entry entry = (Entry)it.next();
+            entry.accept(this);
+        }
+        currentdir = savedir;
     }
 }
